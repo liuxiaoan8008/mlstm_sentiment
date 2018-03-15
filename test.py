82be63e32eee00d7f096dd11a70d74b8abed0635
liuxiaoan8008 <f1321698@gmail.com>
2018-03-15 14:18:29 +0800

    add test

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 #!usr/bin/env python
 # -*- coding:utf-8 -*-
-
+import pandas as pd
+import numpy as np
 from encoder import Model
 
-model = Model('/var/data/mlstm/model/model.npy')
+base_dir = '/var/data/mlstm/'
+
+model = Model(base_dir + 'models/model.npy')
 
 def load_sst(path):
     data = pd.read_csv(path, encoding='utf-8')
@@ -11,9 +14,23 @@ def load_sst(path):
     Y = data['label'].values
     return X, Y
 
-X,Y = load_sst('./data/test.csv')
-Xt = model.transform(X)
-for x in Xt:
-	print x
+
+trX,trY = load_sst('./data/train.csv')
+teX,teY = load_sst('./data/test.csv')
+print trX[0]
+print trY[0]
+
+if not os.path.exists(base_dir + 'features'):
+    os.makedirs(base_dir + 'features')   
+    trXt = model.transform(trX)
+    teXt = model.transform(teX)
+
+    np.save(base_dir + 'features/trXt',trXt)
+    np.save(base_dir + 'features/teXt',trXt)
+
+else:
+	trXt = np.load(base_dir + 'features/trXt.npy')
+    teXt = np.load(base_dir + 'features/teXt.npy')
+
 
 
