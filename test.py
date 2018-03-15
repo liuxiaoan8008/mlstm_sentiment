1c764fb511db3a1a0160d399439101460b2c603a
liuxiaoan8008 <f1321698@gmail.com>
2018-03-15 13:58:31 +0800

    add print

--- test.py
+++ test.py
@@ -5,5 +5,15 @@ from encoder import Model
 
 model = Model('/var/data/mlstm/model/model.npy')
 
-print model.transform(['恩恩好好表现哦'])
+def load_sst(path):
+    data = pd.read_csv(path, encoding='utf-8')
+    X = data['sentence'].values.tolist()
+    Y = data['label'].values
+    return X, Y
+
+X,Y = load_sst('./data/test.csv')
+Xt = model.transform(X)
+for x in Xt:
+	print x
+
 
